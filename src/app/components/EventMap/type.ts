b60fcf34c7f96dcd45630821a1d06ffe19b824f7
Carlo <carlorebuffat412@gmail.com>
2025-07-22 17:29:19 +0200

    small fix

--- src/app/components/EventMap/type.ts
+++ src/app/components/EventMap/type.ts
@@ -1,32 +1,33 @@
 export enum SeatStatus {
-    BOOKED = "booked",
-    HIDE = "hide",
-    AVAILABLE = "free"
+  BOOKED = "booked",
+  HIDE = "hide",
+  AVAILABLE = "free",
 }
 
 export enum SectorColor {
-    SETTORE_2 = "#f7941d",
-    SETTORE_1 = "#af7de1",
-    TRIBUNA_CENTRALE = "#3c9",
-    SETTORE_3 = "#d9cd21",
-    SETTORE_4 = "#d921c9",
-    SETTORE_5 = "#2193d9",
-    DEFAULT = "#062f6e"
+  SETTORE_2 = "#f7941d",
+  SETTORE_1 = "#af7de1",
+  TRIBUNA_CENTRALE = "#3c9",
+  SETTORE_3 = "#d9cd21",
+  SETTORE_4 = "#d921c9",
+  SETTORE_5 = "#2193d9",
+  DEFAULT = "#062f6e",
 }
 
 export interface ISeatData {
-    status: SeatStatus
-    sector: string;
-    name: string
+  status: SeatStatus;
+  sector: string;
+  name: string;
+  number: string;
 }
 
 export interface ISubSectionData {
-    seats_by_rows: {
-        [row: string]: ISeatData[];
-    };
-    settore_name: string;
+  seats_by_rows: {
+    [row: string]: ISeatData[];
+  };
+  settore_name: string;
 }
 
 export interface ISectionData {
-    subsections: ISubSectionData[];
+  subsections: ISubSectionData[];
 }
